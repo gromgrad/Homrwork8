087818501f465623ba4f8e71800e356589adb3a5
gromgrad <snegobur@gmail.com>
2024-11-19 21:40:42 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,15 @@
+#подсчет символов в строке1
+my_string=("Сегодня лучший день:")
+print(len(my_string))
+
+#вывод в верхний и нижний регистр
+print(my_string.upper())
+print(my_string.lower())
+#удаление пробелов
+print(my_string.replace(' ',''))
+# вывод первого символа строки
+print(my_string[0])
+# вывод последнего символа строки
+print(my_string[19])
+
+
